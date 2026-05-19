b730bea71b979b14ff9981e9efb2b3b3a03003a7
yara22aulia <yara22aulia@gmail.com>
2026-05-19 16:36:25 +0700

    membuat class baseclass

--- noPewarisan.cpp
+++ noPewarisan.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 
-class baseClass final {
+class baseClass final 
+{
 public:
     virtual void perkenalan() {
         cout << "Hallo saya Function dari base class";
