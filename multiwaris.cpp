34af334158dc99b023e4a5e1248931f90202b05d
yara22aulia <yara22aulia@gmail.com>
2026-05-19 16:53:49 +0700

    membuat class budi dan mencoba di main

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -32,13 +32,14 @@ public:
     }
 };
 
-class budi : public pekerja, public pelajar {
+class budi : public pekerja, public pelajar 
+{
 public:
 
     budi(int pUmur) :
         pekerja(pUmur),
         pelajar(pUmur),
-        orang(pUmur) //hal ini dapat dilakukan jika menggunakan virtual
+        orang(pUmur) 
     {
         cout << "Budi dibuat\n" << endl;
     }
